f88efb2ed650ed0a09df81d43c84574caf91ecbb
rodneystoleair <dorion40@gmail.com>
2025-09-25 19:41:46 +0300

    123

--- 01_data_loading.R
+++ 01_data_loading.R
@@ -22,60 +22,19 @@ library('tidyverse')
 # 2. Data loading ---- 
 # 2.1. Loading from xlsx-sheets
 
-# Fossil data (prediction set)
-name = readline('Write the exact core name for a reconstruction: ')
-fossil = read_excel(paste0('data/fossil/fossil_', name, '.xlsx')) |>
-  gather(variable, value, -depth) |>
-  spread(depth, value) |>
-  transform(variable = as.numeric(variable)) |>
-  arrange(variable)
-depth = fossil$variable
-ages = read_excel(paste0('data/fossil/ages_', name, '.xlsx'), col_names = F) |> 
-  pull()
-
 # Modern data (training set)
-modern = read_excel('data/modern/modern.xlsx',
-                    col_types = c('text', rep('numeric', 51))) |>
-  as.data.frame() |>
+modern = read_excel('data/modern/modern_empd_example.xlsx') |>
+  as_tibble() |>
   arrange(points)
 
 # Parameters
-# Woody cover
-cover = read_excel('data/parameters/woody_cover.xlsx',
-                   col_types = c('text', rep('numeric', 4), 'text', 'text')) |>
-  as.data.frame() |>
-  arrange(points)
-
 # Climate
-climate = read_excel('data/parameters/climate.xlsx',
-                     col_types = c('text', rep('numeric', 4))) |> 
+climate = read_excel('data/parameters/climate_empd.xlsx') |> 
+  as_tibble() |> 
   arrange(points)
-
-## Biome selection, specific for data used in paper, because biome data of our 
-## training set locates with modern woody cover data (cover.xlsx).
-## Everything specific for paper data and unnecessarily used for a custom data 
-## is marked with doubled hash tag.
-modern = left_join(modern,
-                   cover,
-                   by = 'points') |>
-  select(-km50:-type1) |>
-  filter(!str_detect(type2, 'putorana')) |>
-  select(-type2)
-cover = select(cover, -type1:-type2)
-
-## Column names switch
-row.names(modern) = modern$points
-row.names(fossil) = fossil$variable
-colnames(fossil) = colnames(modern)
-
-# parameters_modern = semi_join(climate, modern, by = 'points') |>
-#   inner_join(cover) |>
-#  select(-km50, -km10, -km20, -km5)
 
 ## Params join
-parameters_modern = semi_join(climate, modern, by = 'points') |>
-  inner_join(cover) |>
-  select(points, km50, km20, km10, km5)
+parameters_modern = semi_join(climate, modern, by = 'points')
 
 parameters_types = colnames(parameters_modern)[-1]
 
